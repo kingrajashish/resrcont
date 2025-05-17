32d821a301e40903d8924585cd094c8bf7d12631
kingrajashish <54197173+kingrajashish@users.noreply.github.com>
2025-05-18 02:19:13 +0530

    Update config.py

--- main/plugins/config.py
+++ main/plugins/config.py
@@ -1,3 +1,3 @@
 OWNER_ID = 1118716436
 MONGODB_CONNECTION_STRING = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn" #change it to yours
-LOG_GROUP = -10012345 # always with -100
+LOG_GROUP = -1002507533596 # always with -100
